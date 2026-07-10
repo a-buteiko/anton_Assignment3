c94fd4c0046a9ec1e3992479e13c4d1b56187a20
a.buteiko <anton.buteiko@gmail.com>
2026-07-10 22:50:50 +0300

    Task_2 Completed!

---
+++ task_2.py
@@ -0,0 +1,24 @@
+deposit_account = {
+"client_id": "C1025",
+"balance": 5000.0,
+"currency": "UAH",
+"interest_rate": 0.08, # 8% річних
+"is_active": True
+}
+
+
+interest = deposit_account["balance"] * deposit_account["interest_rate"]
+
+print(interest)
+
+
+deposit_account["balance"] = deposit_account["balance"] + interest
+
+
+deposit_account["last_update_type"] = "interest accrual"
+
+
+deposit_account["is_active"] = False
+
+
+print(deposit_account)
